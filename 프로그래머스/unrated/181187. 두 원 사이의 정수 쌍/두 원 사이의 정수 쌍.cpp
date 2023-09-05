7da1b334c6eef51a1230bc05181638768a0f1090
KALI-UM <124039541+KALI-UM@users.noreply.github.com>
2023-09-05 15:20:14 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 4.81 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.cpp
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.cpp
@@ -1,6 +1,3 @@
-#include <string>
-#include <vector>
-#include <iostream>
 #include <cmath>
 
 using namespace std;
@@ -11,14 +8,15 @@ long long solution(int r1, int r2) {
     double R1 = pow(r1, 2);
     double R2 = pow(r2, 2);
    
+    //x가 정수값일 때, 원1, 원2의 y값 각각 y1, y2을 구한다
+    //r1원 내 좌표여야 하기 때문에 //r2원 외 좌표여야 하기 때문에
+    //y2보다 작아야 하므로 내림   //y1는 올림 계산을 해서 서로 뺀다.
     for(int x =0; x<r1; x++)
     {
         double y2=0;
         double y1=0;
-       
         y2 = sqrt(R2 - pow(x, 2)); 
         y1 = sqrt(R1 - pow(x, 2));
-        //cout<<"y2-"<<floor(y2)<<" y1-"<<ceil(y1)<<endl;
         answer += floor(y2)-ceil(y1)+1;
     }
     
@@ -26,10 +24,10 @@ long long solution(int r1, int r2) {
     {
         double y=0; 
         y = sqrt(R2 - pow(x, 2)); 
-        //cout<<"y-"<<floor(y)<<endl;
         answer +=floor(y)+1;
-    }
-    
+    }    
+  
+    //1사분면의 결과를 4배 할 것이기 때문에 중복되지 않도록 빼준다.
     answer-=r2-r1+1;
     
     return answer*4;
