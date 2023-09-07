18249979cd993e8d869d7aafc404d276ebc3002b
KALI-UM <124039541+KALI-UM@users.noreply.github.com>
2023-09-07 15:00:30 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
@@ -0,0 +1,69 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> sequence, int k) {
+    vector<int> answer;
+    
+    answer.push_back(0);
+    answer.push_back(0);
+
+    int temp=0;
+    
+    for(int i=sequence.size()-1; i>=0; i--)
+    {
+        if(sequence[i]>k)
+        {
+            continue;
+        }
+        else if(sequence[i]==k)
+        {
+           while(i>0&&sequence[i]==sequence[i-1])
+           {
+               i--;
+           }
+            
+            answer[0]=i;
+            answer[1]=i;
+            
+            break;
+        }
+        else
+        {
+            answer[temp==0? 1: 0]=i;
+            temp+=sequence[i]; 
+
+            if(temp==k)
+           { 
+                if((sequence[answer[0]]==sequence[answer[1]]))
+                {
+                    if(i==0)
+                    {
+                        break;
+                    }
+                    else if(sequence[i]!=sequence[i-1])
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        temp-=sequence[answer[1]];
+                        answer[1]=answer[1]-1; 
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            else if(temp>k)
+            {
+                temp-=sequence[answer[1]];
+                answer[1]=answer[1]-1;
+            }       
+        }   
+    }
+
+    return answer;
+}
