32751cc7ae651093c4f6b3787801c07af0f664bf
KALI-UM <124039541+KALI-UM@users.noreply.github.com>
2023-12-12 15:13:20 +0900

    [Gold I] Title: 구간 합 구하기, Time: 212 ms, Memory: 18864 KB -BaekjoonHub

---
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+vector<long> tree;
+void UpdateTree(int node, long val)
+{
+	long diff = val - tree[node];
+	while (node > 0)
+	{
+		tree[node] = tree[node] + diff;
+		node = node / 2;
+	}
+}
+
+long GetSum(int s, int e)
+{
+	long result = 0;
+
+	while (s <= e)
+	{
+		if (s % 2 == 1)
+		{
+			result += tree[s];
+			s++;
+		}
+		if (e % 2 == 0)
+		{
+			result += tree[e];
+			e--;
+		}
+
+		s = s / 2;
+		e = e / 2;
+	}
+	return result;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	int N, M, K;
+	cin >> N >> M >> K;
+
+	int size = 20;
+	while (N <= (int)pow(2, size))
+		size--;
+
+	size++;
+	int leafIndex = (int)pow(2, size);
+	tree = vector<long>(leafIndex*2, 0);
+	for (int i = 0; i < N; i++)
+		cin >> tree[leafIndex+ i];
+	
+	int index = tree.size() - 1;
+	while (index!=1)
+	{
+		tree[index / 2] += tree[index];
+		index--;
+	}
+
+	vector<long> result;
+	long temp, b, c;
+	for (int i = 0; i < M + K; i++)
+	{
+		cin >> temp >> b >> c;
+		if (temp == 1)
+		{
+			UpdateTree(leafIndex + b - 1, c);
+		}
+		else
+		{
+			result.push_back(GetSum(leafIndex + b - 1, leafIndex + c - 1));
+		}
+	}
+
+	for (long r : result)
+		cout << r << "\n";
+}
