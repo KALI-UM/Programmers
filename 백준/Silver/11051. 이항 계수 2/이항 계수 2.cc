33b6f5d9a8102528460feb8bc6670c7cecc77d20
KALI-UM <124039541+KALI-UM@users.noreply.github.com>
2023-12-14 14:51:58 +0900

    [Silver II] Title: 이항 계수 2, Time: 4 ms, Memory: 5996 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	int N, K;
+	cin >> N >> K;
+
+	vector<vector<int>> table(N + 1, vector<int>(N + 1, 0));
+
+	for (int i = 0; i < N + 1; i++)
+	{
+		table[i][1] = i;
+		table[i][0] = 1;
+		table[i][i] = 1;
+	}
+
+	for (int n = 1; n <= N; n++)
+	{
+		for (int k = 2; k <= n; k++)
+		{
+			table[n][k] = table[n - 1][k] + table[n - 1][k - 1];
+			table[n][k] = table[n][k] % 10007;
+		}
+	}
+	cout << table[N][K]<<"\n";
+}
