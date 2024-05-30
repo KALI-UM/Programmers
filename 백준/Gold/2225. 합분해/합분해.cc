b3fca96dd33f821c6b373d2bec391c085e24a307
KALI-UM <124039541+KALI-UM@users.noreply.github.com>
2024-05-30 18:01:25 +0900

    [Gold V] Title: 합분해, Time: 0 ms, Memory: 2364 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int N;
+int K;
+
+long long DP[210][210];
+long long mod = 1000000000;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N >> K;
+
+	DP[0][0] = 1;
+	DP[1][1] = 1;
+	for (int i = 2; i <= 200; i++)
+	{
+		DP[1][i] = 1;
+		DP[i][1] = i;
+	}
+
+	for (int n = 2; n <= N; n++)
+	{
+		for (int i = 2; i <= K; i++)
+			DP[i][n] += (DP[i][n - 1] + DP[i - 1][n]) % mod;
+	}
+	cout << DP[K][N] << '\n';
+}
